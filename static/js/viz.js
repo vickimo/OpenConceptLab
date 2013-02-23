e37ace7fb017674fc28671504b9f43e3a2172bf6
Victoria Mo <vm2355@columbia.edu>
2013-02-23 14:31:00 -0500

    index

--- static/js/viz.js
+++ static/js/viz.js
@@ -82,24 +82,7 @@ svg.append("text")
     .attr("transform", "rotate(-90)")
     .text("count of ANC 1, 2, 3");
 
-var legend = svg.selectAll(".legend")
-      .data(ageNames.slice().reverse())
-    .enter().append("g")
-      .attr("class", "legend")
-      .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
 
-  legend.append("rect")
-      .attr("x", width - 18)
-      .attr("width", 18)
-      .attr("height", 18)
-      .style("fill", color);
-
-  legend.append("text")
-      .attr("x", width - 24)
-      .attr("y", 9)
-      .attr("dy", ".35em")
-      .style("text-anchor", "end")
-      .text(function(d) { return d; });
 
 d3.selectAll("input").on("change", change);
 
